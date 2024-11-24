7a8990ad4da5ac6375ac6a3669fcb9f3e77f3439
FIZZI-77 <zaprudnovfedor@gmail.com>
2024-11-24 15:20:59 +0300

    Update changed maven add json

--- docker_maven/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-node /usr/src/app/server.js &
-echo "HTTP server started on port 8080"
-
-tail -f /dev/null
---
+++ docker_maven/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Выводим сообщение на экран
+        System.out.println("Hello, World!");
+    }
+}
