a808b81eba6872a13cd2ea9dd04eedaf497baf05
FIZZI-77 <zaprudnovfedor@gmail.com>
2024-11-24 16:05:33 +0300

    Update changed maven add json

--- docker_maven/entrypoint.sh
+++ docker_maven/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Запускаем сервер Node.js в фоновом режиме
-node src/app/server.js &
-
-
-
-# Сообщаем о запуске сервера
+node docker_maven/src/server.js &
 echo "HTTP server started on port 8080"
 
-# Ожидаем завершения серверного процесса (не обязательно)
-wait $(jobs -p)
-
-# Или используем tail для "зависания" контейнера
-# tail -f /dev/null
+tail -f /dev/null
