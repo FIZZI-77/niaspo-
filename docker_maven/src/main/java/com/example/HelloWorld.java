f73a5e8af06ab66efdb6e286a96d36a2095f02b7
FIZZI-77 <zaprudnovfedor@gmail.com>
2024-11-24 15:28:51 +0300

    Update changed maven add json

--- docker_maven/src/main/java/com/example/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Выводим сообщение на экран
-        System.out.println("Hello, World!");
-    }
-}
